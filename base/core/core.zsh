4a865c9c59aa0eee921c0071c60321e0facdc361
Naoki Mizuno <nigorojr@gmail.com>
2016-11-17 22:23:24 +0900

    Add support for zim

--- base/core/core.zsh
+++ base/core/core.zsh
@@ -178,6 +178,7 @@ __zplug::core::core::variable()
     typeset -gx    _ZPLUG_URL="https://github.com/zplug/zplug"
     typeset -gx    _ZPLUG_OHMYZSH="robbyrussell/oh-my-zsh"
     typeset -gx    _ZPLUG_PREZTO="sorin-ionescu/prezto"
+    typeset -gx    _ZPLUG_ZIM="Eriner/zim"
     typeset -gx    _ZPLUG_AWKPATH="$ZPLUG_ROOT/misc/contrib"
 
     # user-defined exit code 64..113
